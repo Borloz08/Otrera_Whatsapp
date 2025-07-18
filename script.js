cabd3d90f4d2da34f9faf2b731a1e3da3511943d
Borloz08 <andres.borloz@estudiantec.cr>
2025-07-17 22:18:48 -0600

    cambios web

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("registro-form").addEventListener("submit", async functi
   const pais = document.getElementById("pais").value;
   const telefono = document.getElementById("telefono").value;
 
-  const response = await fetch("/api/registrar", {
+  const response = await fetch("https://otrera-servidor.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
